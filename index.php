c868a5173d5de07e667fd6049dd4478a705d3af6
vinicius <daftwarz@gmail.com>
2022-11-26 23:35:03 -0300

    portifolio

---
+++ index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/font-awesome.min.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital@1&display=swap" rel="stylesheet">
+    <link href="estilo/style.css" rel="stylesheet">
+     <title>addaa</title>
+</head>
+<body>
+<header class="header">
+<p class="fechar"><i class="fa fa-bars" aria-hidden="true"></i></p>  
+
+
+<nav class="nav"> 
+
+<a href="#">Inicio</a>
+<a href="#">Perfil</a>
+<a href="#">Área de Atuação</a>
+<a href="#">Contato</a>
+</header>
+<div class="center">
+<section class="ada">
+
+<img class="img" src="images/aaaa.png">
+
+
+
+
+<h3>Perfil</h3>
+<p> Meu nome é josé vinicius, 25 anos, graduando em Ciências da Computação
+
+
+<h3>Area de Atuação</h3>
+<p>Desenvolvimento FullStack
+
+
+
+<h3>Contato</h3>
+<p>Linkedin : <a href="www.linkedin.com/in/viniciecomp">www.linkedin.com/in/viniciecomp</a></p>
+
+</section>
+</div>
+<div class="grid">
+    <div class="end">
+        <img class="img_grid" src="images/html.png">
+    </div>
+    <div>
+        <img class="img_grid" src="images/css.png">
+    </div>
+    <div>
+        <img class="img_grid" src="images/js.png">
+    </div>
+    <div>
+        <img class="img_grid" src="images/java.png">
+    </div>
+    <div>
+        <img class="img_grid" src="images/php.png">
+    </div> 
+    <div>
+    <img class="img_grid" src="images/database.png">
+    </div>        
+</div>
+<footer class="footer">
+    <div class="content">
+    <h1 class="text">... <h1>
+   
+    </div>
+</footer>
+<script src="js/acao.js"></script>
+
+</body>
+</html>
---
+++ js/acao.js
@@ -0,0 +1,9 @@
+const acao = document.querySelector('.fechar');
+const nav = document.querySelector('.nav');
+
+acao.addEventListener('click', () =>{
+    console.log('cliquei');
+
+        nav.classList.toggle('navOff2');
+    
+})
